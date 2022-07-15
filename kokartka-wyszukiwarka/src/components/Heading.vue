057aa4af694f96e2decd1531b20f899b3f253a99
olkens <olkusz.michal@gmail.com>
2022-07-15 18:35:38 +0200

    hwading changes

--- kokartka-wyszukiwarka/src/components/Heading.vue
+++ kokartka-wyszukiwarka/src/components/Heading.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="box-cont">
-    <div class="box"><p>GRUPA</p></div>
+    <div class="">
+      <!-- <label for="group">GRUPA</label> -->
+      <select name="group" id="group">
+        <option value="grupa">Grupa</option>
+        <option value="mucha">mucha</option>
+        <option value="kokartka">kokartka</option>
+      </select>
+    </div>
     <div class="box"><p>POZIOM</p></div>
     <div class="box"><p>WIEK</p></div>
     <div class="box">SZKOŁA</div>
@@ -21,11 +28,31 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+  border-radius: 2px;
+  color: #fff;
 }
 .box-cont {
   display: flex;
   flex-direction: row;
   gap: 5px;
   margin-bottom: 25px;
+}
+
+select {
+  width: 150px;
+  height: 50px;
+  background: #2e3a59;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2px;
+  color: #fff;
+  text-transform: uppercase;
+  border: none;
+  text-align: center;
+}
+
+option {
+  padding: 5px;
 }
 </style>
